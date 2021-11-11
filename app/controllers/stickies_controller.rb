2842a6286475447f984e3855d60d0ddb9bdd002e
Ben Orr <benjaminbordwell@gmail.com>
2021-11-11 10:38:02 -0500

    Version 1, made changes to controller, added create function, really just pushing in case something didn't get saved

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -1,5 +1,14 @@
 class StickiesController < ApplicationController
   def index
+    @all_stickies = Stickies.all
+  end
+
+  def create
+    new_sticky = Sticky.new
+    new_sticky.title = params["title"]
+    new_sticky.body = params["body"]
+
+    new_sticky.save
   end
 
 end
